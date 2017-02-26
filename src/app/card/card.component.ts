12b4f108c0840719d454e5e846ea75627392b733
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-02-26 12:01:39 +0300

    card component

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: [ 'card.component.css' ]
+})
+export class CardComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
